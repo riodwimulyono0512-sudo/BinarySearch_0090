5c2399fde39bd698ab6f996594704d755243c230
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-04-16 18:17:05 +0700

    data 8

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -67,3 +67,15 @@ void display()
     }
     cout << endl;
 }
+
+void binarySearch()
+{
+    char ulang;
+    do
+    {
+        cout << "\n==================================\n";
+        cout << "       Pencarian Binary Search      \n";
+        cout << "====================================\n";
+
+        cout << "Masukkan elemen yang ingin dicari: ";
+        cin >> x;
