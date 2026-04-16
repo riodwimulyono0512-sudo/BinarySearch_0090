c7c60eccb9b1db462eb6620e4c109af61c691859
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-04-16 18:18:04 +0700

    data 10

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -86,3 +86,29 @@ void binarySearch()
         do
         {
             int mid = (low + high) / 2;
+
+             if (element[mid] == x)
+            {
+                cout << "\n[v] Elemen " << x << " ditemukan pada indeks " << mid << "\n";
+                return;
+            }
+            if (x < element[mid]);
+            {
+                high = mid - 1;
+            }
+            if (x > element[mid]);
+            {
+                low = mid +1;
+            }
+        } while (low <= high);
+        
+        if (low <= high)
+        {
+            cout << "\n[x] Elemen " << x << " tidak ditemukan dalam array.\n";
+        }
+
+        cout << "\nIngin mencari lagi? (y/n): ";
+        cin >> ulang;
+        
+    } while (ulang == 'y' || ulang == 'Y');
+}
