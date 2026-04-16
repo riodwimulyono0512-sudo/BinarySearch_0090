5b230db7fb63e78cd465252dc49932683f267cea
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-04-16 18:16:42 +0700

    data 7

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -56,3 +56,14 @@ void display()
     cout << "\n====================================\n";
     cout << "  Elemen Array Setelah Diurutkan (Asc)\n";
     cout << "======================================\n";
+
+     for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
+}
